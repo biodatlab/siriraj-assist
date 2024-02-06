e682a19f47ed4f3c488d7a1df8683575aac67810
boom4869 <144575647+boom4869@users.noreply.github.com>
2024-02-07 01:10:22 +0700

    Update chatbot_script.py

--- chatbotscript/chatbot_script.py
+++ chatbotscript/chatbot_script.py
@@ -1,73 +1,40 @@
-import openai
 import streamlit as st
+import openai
 from llama_index import (
     KeywordTableIndex,
-    SimpleDirectoryReader,
     ServiceContext,
 )
-from llama_index import VectorStoreIndex,download_loader,get_response_synthesizer
-from llama_index.output_parsers import LangchainOutputParser
-from llama_index.llms import OpenAI
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from llama_index import VectorStoreIndex,download_loader
 from pathlib import Path
 from llama_index.retrievers import VectorIndexRetriever
-from llama_index.query_engine import RetrieverQueryEngine
-from llama_index.postprocessor import SimilarityPostprocessor
-from llama_index.chat_engine.condense_question import CondenseQuestionChatEngine
-import json
+from llama_index.llms import OpenAI
 
-OPENAI_API_KEY = "sk-...-..."  # put your API key here
-FILE_PATH =  "./siriraj_doctor_details.jsonl"  # put a path to JSONL here
 
-openai.api_key = OPENAI_API_KEY
+
+openai.api_key = "...."
 st.set_page_config(page_title="Chatbot for doctor appointment", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Chatbot for doctor appointment")
 st.info("แชทบอทช่วยตอบคำถามสำหรับการนัดหมายแพทย์ที่โรงพยาบาลศิริราช ปิยมหาราชการุณย์ ดูข้อมูลแพทย์เพิ่มเติมได้ที่ (https://www.siphhospital.com/th/medical-services/find-doctor)", icon="📃")
-prompt = """
-Given the following doctors' data in this format, craft a response in Thai to a patient asking about scheduling an appointment, inquiring about the doctor's expertise, or seeking a recommendation for a doctor based on their needs:
 
-{
-  "name": "...",
-  "image_src": "...",
-  "url": "...",
-  "table_check": "...",
-  "qualification": "...",
-  "expertise": "..."
-}
+system_prompt = """
+Given the following doctors' data in the CSV file and embedded, create a response in Thai to a patient asking about scheduling an appointment, inquiring about the doctor's expertise, or seeking a recommendation for a doctor based on their needs. Use only the data provided. The response should be in Thai and do not hallucinate. If the patient wants to make an appointment, create a CSV file content for a list of doctor appointments. The CSV should include columns for Doctor's Name, Appointment Time, Appointment Date, and Expertise. Ensure that the appointment times and dates match with the database. Format the data to align with standard CSV formatting conventions, including headers and comma-separated values. Provide the content as plain text that can be copied into a CSV file and confirm with the patient.
+"""
 
-Use only the data provided in the JSONL file in the index. The response should be in Thai and tailored to the specific inquiry, whether it's about scheduling an appointment, the doctor's expertise, or a suggestion for a doctor based on the patient's condition or needs."""
-
-
+llm= OpenAI(model="gpt-4",system_prompt=system_prompt,temperature=0.3)
+service_context = ServiceContext.from_defaults(llm=llm)
 @st.cache_resource(show_spinner=False)
-def load_data(file_path: str = "./siriraj_doctor_details.jsonl"):
+
+def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        JSONReader = download_loader("JSONReader")
-        loader = JSONReader()
-        docs = loader.load_data(Path(file_path), is_jsonl=True)
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt= prompt ))
+        file_path = "./data/siriraj_doctor_details(2).csv"
+        PandasCSVReader = download_loader("PandasCSVReader")
+        loader = PandasCSVReader()
+        docs = loader.load_data(file=Path(file_path))
         index = VectorStoreIndex.from_documents(docs,service_context=service_context)
-        return index
+    return index
 
-
-index = load_data(FILE_PATH)
-retriever = VectorIndexRetriever(
-    index=index,
-    similarity_top_k=10,
-)
-
-# configure response synthesizer
-response_synthesizer = get_response_synthesizer()
-
-# assemble query engine
-query_engine = RetrieverQueryEngine(
-    retriever=retriever,
-    response_synthesizer=response_synthesizer,
-    node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
-)
-chat_engine = CondenseQuestionChatEngine.from_defaults(
-    query_engine=query_engine,
-    verbose=True,
-)
+index = load_data()
+chat_engine = index.as_chat_engine(chat_mode="context")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
@@ -92,3 +59,4 @@ if messages[-1]["role"] != "assistant":
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
+
